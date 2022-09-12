9ae37581daeb88c93a11597dbf8015415f2b6a47
Wokcito <maxikazanski@gmail.com>
2022-09-12 16:21:19 -0300

    add api: create an api using json server

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Components
+import Header from "./components/Header";
+import Productos from "./components/Productos";
+import NuevoProducto from "./components/NuevoProducto";
+import EditarProducto from "./components/EditarProducto";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Header />
+
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<Productos />} />
+                    <Route path="/productos/nuevo" element={<NuevoProducto />} />
+                    <Route path="/productos/editar/:id" element={<EditarProducto />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ src/components/NuevoProducto.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const NuevoProducto = () => {
+    return (
+        <div className="row justify-content-center">
+            <div className="col-md-8 ">
+                <div className="card p-2">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">Agregar Nuevo Producto</h2>
+                    </div>
+
+                    <form>
+                        <div>
+                            <label>Nombre Producto</label>
+                            <input type="text" className="form-control" placeholder="Nombre Producto" name="nombre" />
+                        </div>
+                        <div>
+                            <label>Precio Producto</label>
+                            <input type="number" className="form-control" placeholder="Precio Producto" name="precio" />
+                        </div>
+                        <button type="submit" className="btn btn-primary font-weight-bold text-uppercase d-block w-100 m-1">
+                            AGREGAR
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NuevoProducto;
