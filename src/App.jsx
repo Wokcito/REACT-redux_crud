410ee8aa6d92a1d77145c41afc4c32dbf3536efd
Wokcito <maxikazanski@gmail.com>
2022-09-13 00:28:00 -0300

    add actions

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,24 @@ import Productos from "./components/Productos";
 import NuevoProducto from "./components/NuevoProducto";
 import EditarProducto from "./components/EditarProducto";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
     return (
         <BrowserRouter>
-            <Header />
+            <Provider store={store}>
+                <Header />
 
-            <div className="container">
-                <Routes>
-                    <Route path="/" element={<Productos />} />
-                    <Route path="/productos/nuevo" element={<NuevoProducto />} />
-                    <Route path="/productos/editar/:id" element={<EditarProducto />} />
-                </Routes>
-            </div>
+                <div className="container">
+                    <Routes>
+                        <Route path="/" element={<Productos />} />
+                        <Route path="/productos/nuevo" element={<NuevoProducto />} />
+                        <Route path="/productos/editar/:id" element={<EditarProducto />} />
+                    </Routes>
+                </div>
+            </Provider>
         </BrowserRouter>
     );
 }
